d016a5aa67d531ef2930bc62d7958f36b91a92c1
Yvo van Doorn <yvo@opscode.com>
2014-04-02 19:03:13 -0700

    add environment vars to make tc server delightful \(maybe\)

--- providers/ctl.rb
+++ providers/ctl.rb
@@ -3,7 +3,7 @@
 action :start do
   execute "start_#{new_resource.instance_name}" do
     cwd '/opt/vmware/vfabric-tc-server-standard'
-    command "JAVA_HOME='/usr/lib/jvm/java' /opt/vmware/vfabric-tc-server-standard/tcruntime-ctl.sh #{new_resource.instance_name} start -i /opt/vmware/vfabric-tc-server-standard -d /opt/vmware/vfabric-tc-server-standard && sleep 5"
+    command "TOMCAT_USER='root' TC_SERVER_HOME='/opt/vmware/vfabric-tc-server-standard' INSTANCE_BASE='$TC_SERVER_HOME' INSTANCE_NAME='#{new_resource.instance_name}' JAVA_HOME='/usr/lib/jvm/java' /opt/vmware/vfabric-tc-server-standard/tcruntime-ctl.sh #{new_resource.instance_name} start -i /opt/vmware/vfabric-tc-server-standard -d /opt/vmware/vfabric-tc-server-standard && sleep 5"
     # command "env"
     creates "/opt/vmware/vfabric-tc-server-standard/#{new_resource.instance_name}/logs/tcserver.pid"
   end
